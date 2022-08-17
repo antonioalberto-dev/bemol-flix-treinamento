b9239abc6aae5a88362a270f075538c94b335edb
Antonio Alberto <antonioalbertodsb@gmail.com>
2022-08-17 09:25:23 -0400

    Consumindo API TMDB

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,11 @@
-const url = "https://api.football-data.org/v4/matches";
+const API_KEY = "api_key=718ff094890773202d9924c2fd6a7f98";
+const URL_BASE = "https://api.themoviedb.org/3"
+const URL_API = URL_BASE + "/discover/movie?sort_by=popularity.desc&" + API_KEY;
+
+const div = document.querySelector('.lista-produtos')
+
+fetch(URL_API)
+  .then((resposta) => {
+    return resposta.json();
+  })
+  .then((resposta) => console.log(resposta));
